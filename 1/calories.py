96dce4addef702ada7a6a7d0b5cc9eb06eb48edf
paddya <patrick.bisenius@gmail.com>
2022-12-01 23:11:26 +0100

    Use `sorted()` instead of `.sort()`.

--- 1/calories.py
+++ 1/calories.py
@@ -1,11 +1,9 @@
 
 # Compute the total for all elves first
-elves = [
+elves = sorted([
     sum([int(k) for k in e.split('\n')]) 
     for e in open('input.txt').read().strip().split('\n\n')
-]
-
-elves.sort(reverse=True)
+], reverse=True)
 
 # After sorting, the list can be used to print solutions for both parts
 print(elves[0])
