863383e82795615fed9130a24ac758c9001d40e4
paddya <patrick.bisenius@gmail.com>
2022-12-25 14:54:19 +0100

    Day 25.

---
+++ 25/snafu.py
@@ -0,0 +1,85 @@
+import math
+import sys
+
+
+def fromSnafu(snafu):
+    exp = len(snafu) - 1
+    
+    mapping = {
+        '0': 0,
+        '1': 1,
+        '2': 2,
+        '-': -1,
+        '=': -2
+    }
+        
+    total = 0
+    for digit in snafu:
+        decimal = mapping[digit]
+        total += decimal * 5**exp
+        exp -= 1
+    return total
+        
+
+# Computes the maximum number that can be formed with all exponents 0 <= exp < maxExponent
+def exclusiveMax(maxExponent):
+    return sum(2*(5**exp) for exp in range(maxExponent))
+
+EXMAX = dict()
+for i in range(100):
+    EXMAX[i] = exclusiveMax(i)
+
+def toSnafu(num):
+    maxExponent = 0
+    
+    # Compute the maximum exponent required
+    while True:
+        localMax = EXMAX[maxExponent]
+        # The previous exponent was good enough, since we are now over
+        if localMax >= abs(num):
+            maxExponent = maxExponent - 1
+            break
+        maxExponent += 1
+        
+    curExp = maxExponent
+    
+
+    
+    mapping = {
+        -2: '=',
+        -1: '-',
+        0: '0',
+        1: '1',
+        2: '2'
+    }
+    
+    # Start with the number itself, then find each digit from left to right and subtract digit * 5**curExp every time
+    rem = num
+    digits = []
+    while curExp >= 0:      
+        absRem = abs(rem)
+        digit = 0
+                
+        # There probably is a solution without a loop here...
+        while EXMAX[curExp] < absRem - digit*5**curExp:
+            digit += 1
+
+        if rem < 0:
+            digit *= -1
+        rem = rem - digit*5**curExp
+        print(digit, d, r, rem)
+        assert digit == d and r == rem
+        digits.append(mapping[digit])
+        curExp -= 1
+        
+    return ''.join(digits)
+    
+    
+ 
+# Roundtrips a decimal number and checks if the result matches   
+def roundTrip(num):
+    snafu = toSnafu(num)
+    print(str(num), ' => ', snafu)
+    return fromSnafu(snafu) == num
+
+print(toSnafu(sum(fromSnafu(l) for l in open(sys.argv[1]).read().split('\n'))))
